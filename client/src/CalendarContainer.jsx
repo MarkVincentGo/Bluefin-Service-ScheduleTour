99c587e54388c37c4690fb7fbc20ffaf05579e40
Mark Go <mark.vince.go@gmail.com>
2020-01-28 20:31:55 -0800

    Added method to calculate date for the next two weeks.(not leap year). Able to pass on the date date to props. Next step is to make a calendar entry component

--- client/src/CalendarContainer.jsx
+++ client/src/CalendarContainer.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default const CalendarContainer = (props) => (
-  
-)
+const CalendarContainer = (props) => (
+  <div>
+    {props.date.day.map((day, i) => (
+      <>
+        <div>{day}</div>
+        <div>{props.date.month[i]}</div>
+        <div>{props.date.date[i]}</div>
+      </>
+    ))}
+  </div>
+);
+
+export default CalendarContainer;
--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,8 @@
   import React, { Component } from 'react';
   import axios from 'axios';
 
+  import CalendarContainer from './CalendarContainer'
+
   export default class App extends Component {
     constructor(props) {
       super(props);
@@ -22,12 +24,65 @@
       };
     };
 
+    getDates() {
+      const date = new Date().getDate();
+      const day = new Date().getDay();
+      const month = new Date().getMonth();
+
+      // the above variables are read only, so I have to make a writable copy of them below
+      let dateWrite = date;
+      let dayWrite = day;
+      let monthWrite = month;
+
+      const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+      const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+      let nextTwoWeeks = {
+        day: [],
+        month: [],
+        date: []
+      }
+
+      const months31 = [0, 2, 4, 6, 7, 9, 11];
+
+      for (let i = 0; i < 14; i += 1) {
+        // this block deals with the days
+        nextTwoWeeks.day.push(days[dayWrite]);
+        dayWrite += 1;
+        if (dayWrite === 7) {dayWrite = 0};
+
+        // this blick deals with months
+        nextTwoWeeks.month.push(months[monthWrite]);
+
+        // this block deals with the dates
+        console.log(monthWrite, dateWrite, months31.includes(monthWrite))
+        nextTwoWeeks.date.push(dateWrite)
+          dateWrite += 1;
+          // if month is february
+        if (monthWrite === 1 && dateWrite === 29) {
+          dateWrite = 1;
+          monthWrite += 1;
+        } else if (months31.includes(monthWrite) && dateWrite === 32) {
+          dateWrite = 1;
+          monthWrite += 1;
+          if (monthWrite === 12) {monthWrite = 0};
+        } else if (months31.indexOf(monthWrite) === -1 && dateWrite === 31) {
+          dateWrite = 1;
+          monthWrite += 1;
+        };
+      }
+
+      return nextTwoWeeks;
+    };
+
     render() {
       return (
         <div>
-          Mamba4Life
+          <CalendarContainer date={this.getDates()}/>
           <p>{
-            this.state.listingId === null ? 'Hello' : `${this.houseData.name}`}
+            this.state.listingId === null ? 'Hello' : this.houseData.name
+            }
           </p>
         </div>
       )
